4d4da9192c69c4a63aee4e26300ff6893db96768
Ange DJEMO <ange.djemo@yahoo.fr>
2023-08-30 20:56:48 +0200

    modif test

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -10,6 +10,7 @@ from flask_login import (
     logout_user
 )
 
+
 from apps import db, login_manager
 from apps.authentication import blueprint
 from apps.authentication.forms import LoginForm, CreateAccountForm
